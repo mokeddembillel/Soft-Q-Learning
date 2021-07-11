a092a1a25dd10f6e11be8ca44766b2cdab5823a3
billel mokeddem <bilel.crysis3@gmail.com>
2021-07-11 18:18:47 +0100

    fix some compatibility things

--- plotter.py
+++ plotter.py
@@ -1,11 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import torch 
+import torch as T
 
 class QFPolicyPlotter:
-    def __init__(self, qf, ss, obs_lst, default_action, n_samples):
+    def __init__(self, qf, policy, obs_lst, default_action, n_samples):
         self._qf = qf
-        self._ss = ss
+        self._policy = policy
         self._obs_lst = obs_lst
         self._default_action = default_action
         self._n_samples = n_samples
@@ -18,12 +18,12 @@ class QFPolicyPlotter:
         x_size = 5 * n_plots
         y_size = 5
 
-        fig = plt.figure(figsize=(x_size, y_size))
+        self.fig = plt.figure(figsize=(x_size, y_size))
         self._ax_lst = []
         for i in range(n_plots):
-            ax = fig.add_subplot(100 + n_plots * 10 + i + 1)
-            ax.set_xlim((-2, 2))
-            ax.set_ylim((-2, 2))
+            ax = self.fig.add_subplot(100 + n_plots * 10 + i + 1)
+            ax.set_xlim((-1, 1))
+            ax.set_ylim((-1, 1))
             ax.grid(True)
             self._ax_lst.append(ax)
 
@@ -35,6 +35,7 @@ class QFPolicyPlotter:
         self._line_objects = list()
 
         self._plot_level_curves()
+
         self._plot_action_samples()
 
         plt.draw()
@@ -42,8 +43,8 @@ class QFPolicyPlotter:
 
     def _plot_level_curves(self):
         # Create mesh grid.
-        xs = np.linspace(-2, 2, 50)
-        ys = np.linspace(-2, 2, 50)
+        xs = np.linspace(-1, 1, 50)
+        ys = np.linspace(-1, 1, 50)
         xgrid, ygrid = np.meshgrid(xs, ys)
         N = len(xs)*len(ys)
 
@@ -52,11 +53,11 @@ class QFPolicyPlotter:
         actions = np.tile(self._default_action, (N, 1))
         actions[:, self._var_inds[0]] = xgrid.ravel()
         actions[:, self._var_inds[1]] = ygrid.ravel()
-        actions = torch.from_numpy(actions.astype(np.float32))
+        actions = T.from_numpy(actions.astype(np.float32))
         for ax, obs in zip(self._ax_lst, self._obs_lst):
-            obs = torch.FloatTensor(obs).repeat([actions.shape[0],1])
-            with torch.no_grad():
-                qs = self._qf(obs, actions).numpy()
+            obs = T.FloatTensor(obs).repeat([actions.shape[0],1])
+            with T.no_grad():
+                qs = self._qf(obs.to(self._qf.device), actions.to(self._qf.device)).cpu().detach().numpy()
 
             qs = qs.reshape(xgrid.shape)
 
@@ -67,10 +68,8 @@ class QFPolicyPlotter:
 
     def _plot_action_samples(self):
         for ax, obs in zip(self._ax_lst, self._obs_lst):
-            with torch.no_grad():
-                actions = self._ss(torch.FloatTensor(obs).repeat([self._n_samples,1])).numpy().squeeze()
-                #print(actions)
+            with T.no_grad():
+                actions = self._policy(T.FloatTensor(obs).repeat([self._n_samples,1]).to(self._policy.device)).cpu().detach().numpy()
             x, y = actions[:, 0], actions[:, 1]
+            ax.title.set_text(str(obs))
             self._line_objects += ax.plot(x, y, 'b*')
-
-
